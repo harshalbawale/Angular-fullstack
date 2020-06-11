416deaf4ec8f01655dc707fd261420490945036f
harshalbawale <harshal.bawale@gmail.com>
2020-06-11 22:33:21 +0530

    Angular first project

---
+++ angular/Angular training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Harshal","Bawale","hsbawale@gmail.com",200),
+    new SalesPerson("abcd","xyz","abcd@gmail.com",100),
+    new SalesPerson("aaa","bbbb","asd@aff.com",300)
+  ];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
